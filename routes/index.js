c1cdc1f4d18acdab2e3a62bd66742f275bcf16b5
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-05-15 10:38:17 +0000

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,9 @@ const router = express.Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
+
 router.post('/users', UsersController.postNew);
+
 router.get('/connect', AuthController.getConnect);
 router.get('/disconnect', AuthController.getDisconnect);
 router.get('/users/me', UsersController.getMe);
@@ -15,5 +17,10 @@ router.post('/files', FilesController.postUpload);
 
 router.get('/files/:id', FilesController.getShow);
 router.get('/files', FilesController.getIndex);
+
+router.put('/files/:id/publish', FilesController.putPublish);
+router.put('/files/:id/unpublish', FilesController.putUnpublish);
+
+router.get('/files/:id/data', FilesController.getFile);
 
 export default router;
