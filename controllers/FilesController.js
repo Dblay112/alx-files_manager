7f354d5587fb832d5ac61f46c4cad78dbaf133f0
Bright Djogatse <125486553+Dblay112@users.noreply.github.com>
2024-05-15 10:37:05 +0000

    Update FilesController.js

--- controllers/FilesController.js
+++ controllers/FilesController.js
@@ -90,3 +90,131 @@ class FilesController{
 
 
     }
+    static async getShow(request, response){
+        //retrieve user based on token
+        
+        const token = request.headers['x-token']
+        const key = `auth_${token}`
+        console.log('key: '+key)
+        const userId = await redisClient.get(key)
+        console.log('userId: ' + userId)
+        if (!userId){
+            return response.status(401).send('error: Unauthorized1')
+        }
+
+        const id = request.params.id
+        console.log('id:'+ id)
+        const collection = dbClient.db.collection('files')
+        const file =  await collection.findOne({_id: new mongo.ObjectID(id) })
+        console.log('file'+file)
+        if (!file){
+            return response.status(404).send('error: Not found')
+
+        }
+        return response.status(200).send({
+            "id": file._id,
+            "userId": file.userId,
+            "name": file.name,
+            "type": file.type,
+            "isPublic":file.isPublic,
+            "parentId": file.parentId,
+        })
+    }
+    static async getIndex(request, response){
+        
+        const token = request.headers['x-token']
+        const key = `auth_${token}`
+        const userId = await redisClient.get(key)
+        
+        if (!userId){
+            return response.status(401).send('error: Unauthorized1')
+        }
+        const parentId = request.query.parentId || 0
+
+    }
+
+
+    static async putPublish(request, response){
+        
+        const token = request.headers['x-token']
+        const key = `auth_${token}`
+        const userId = await redisClient.get(key)
+        if (!userId){
+            return response.status(401).send('error: Unauthorized1')
+        }
+        const id = request.params.id
+        const collection = dbClient.db.collection('files')
+        const file =  await collection.findOne({_id: new mongo.ObjectID(id) })
+        if (!file){
+            return response.status(404).send('error: Not found')
+
+        }
+        file.isPublic = true
+        return response.status(200).send({
+            "id": file._id,
+            "userId": file.userId,
+            "name": file.name,
+            "type": file.type,
+            "isPublic":file.isPublic,
+            "parentId": file.parentId,
+        })
+
+    }
+    static async putUnpublish(request, response){
+        
+        const token = request.headers['x-token']
+        const key = `auth_${token}`
+        const userId = await redisClient.get(key)
+        if (!userId){
+            return response.status(401).send('error: Unauthorized1')
+        }
+        const id = request.params.id
+        const collection = dbClient.db.collection('files')
+        const file =  await collection.findOne({_id: new mongo.ObjectID(id) })
+        if (!file){
+            return response.status(404).send('error: Not found')
+
+        }
+        file.isPublic = false
+        return response.status(200).send({
+            "id": file._id,
+            "userId": file.userId,
+            "name": file.name,
+            "type": file.type,
+            "isPublic":file.isPublic,
+            "parentId": file.parentId,
+        })
+    }
+
+    static async getFile(request, response){       
+        const token = request.headers['x-token']
+        const key = `auth_${token}`
+        const userId = await redisClient.get(key)
+        if (!userId){
+            return response.status(401).send({"error": "Not found"})
+        }
+        const id = request.params.id
+        console.log('id:'+ id)
+        const collection = dbClient.db.collection('files')
+        const file =  await collection.findOne({_id: new mongo.ObjectID(id) })
+        if (!file){
+            return response.status(404).send({"error": "Not found"})
+
+        }
+
+        if (file.isPublic === false && userId != file.userId.toString()){
+            return response.status(404).send({"error": "Not found"})
+        }
+        if (file.type === 'folder'){
+            return response.status(400).send({"error": "A folder doesn't have content"})
+        }
+        if (!fs.existsSync(file.localPath)){
+            return response.status(404).send({"error": "Not found"})
+        }
+        return response.status(200).send(fs.readFileSync(file.localPath))
+
+    }
+
+}
+
+module.exports = FilesController;
